fa1e8e497e482b620eae20d13b7afbf995666230
zebi31 <kilian.djouber2@gmail.com>
2024-09-30 15:51:43 +0200

    act4

---
+++ coda-c-j3/act4/act4.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+int main()
+{
+
+	
+	int a=0;
+	int i=0;
+	while (i!=111222){
+    	a=a+i;
+	i=i+1;}
+	printf("%d",a);
+exit(0);
+}
